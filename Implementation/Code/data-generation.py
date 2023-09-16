212a544530a8804392abe91286ffe08e05089e5e
David Angel Alba Bonilla <78463209+DavidAlba2627@users.noreply.github.com>
2023-09-15 19:37:29 -0600

    Update data-generation.py

--- Implementation/Code/data-generation.py
+++ Implementation/Code/data-generation.py
@@ -1,10 +1,8 @@
 import numpy as np
 from sklearn.datasets import make_blobs
 
-# Choosing parameters
-Num_points = 10000
-k = 5
-np.random.seed(0)
-
-# Creating dataset
-X, y_true = make_blobs(n_samples=Num_points, centers=k, cluster_std=0.8, random_state=3)
+def generate_data(Num_points=10000, k=5):
+    np.random.seed(0)
+    # Creating dataset
+    X, y_true = make_blobs(n_samples=Num_points, centers=k, cluster_std=0.8, random_state=3)
+    return X, y_true
