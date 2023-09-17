2513486a4902a43b4a7cf803570116b8d40b4c55
David Angel Alba Bonilla <78463209+DavidAlba2627@users.noreply.github.com>
2023-09-16 19:41:34 -0600

    Update plotting_results.py

--- Implementation/Code/plotting_results.py
+++ Implementation/Code/plotting_results.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-def plot_clusters(X, Labels, Centroids, title="Clustering Results"):
+def plot_clusters(X, Labels, Centroids, k=5, title="Clustering Results"):
     plt.figure(figsize=(7, 5))
-    plt.scatter(X[:, 0], X[:, 1], c=Labels, cmap='spring', label="Clusters")
-    plt.scatter(Centroids[:, 0], Centroids[:, 1], c='black', s=200, alpha=0.5, label="Centroids")
+    scatter = plt.scatter(X[:, 0], X[:, 1], c=Labels, cmap='spring')
+    plt.scatter(Centroids[:, 0], Centroids[:, 1], c='black', s=200, alpha=0.5)
     plt.title(title)
-    plt.legend()  # Display legend
+    plt.legend(handles=scatter.legend_elements()[0], labels=range(k), title='Clusters')
     plt.show()
 
